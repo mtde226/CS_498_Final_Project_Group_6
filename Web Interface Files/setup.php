3afe9d9bc3d961accfe3def8c293c7ced0a174d7
Jessica Reid <jlre249@cs.uky.edu>
2019-04-06 23:04:45 -0400

    restructuring json attributes

--- Web Interface Files/setup.php
+++ Web Interface Files/setup.php
@@ -2,8 +2,49 @@
 
 $student->username = $_POST["name"];
 $student->courses = array($_POST["course1"],$_POST["course2"],$_POST["course3"],$_POST["course4"],$_POST["course5"],$_POST["course6"]);
-$student->assignments = array();
-$student->grades = array();
+
+$student->courses[$_POST["course1"]]->assignments = array();
+$student->courses[$_POST["course1"]]->exams = array();
+$student->courses[$_POST["course1"]]->quizzes = array();
+$student->courses[$_POST["course1"]]->aweight = $_POST[];
+$student->courses[$_POST["course1"]]->eweight = $_POST[];
+$student->courses[$_POST["course1"]]->qweight = $_POST[];
+
+$student->courses[$_POST["course2"]]->assignments = array();
+$student->courses[$_POST["course2"]]->exams = array();
+$student->courses[$_POST["course2"]]->quizzes = array();
+$student->courses[$_POST["course2"]]->aweight = $_POST[];
+$student->courses[$_POST["course2"]]->eweight = $_POST[];
+$student->courses[$_POST["course2"]]->qweight = $_POST[];
+
+$student->courses[$_POST["course3"]]->assignments = array();
+$student->courses[$_POST["course3"]]->exams = array();
+$student->courses[$_POST["course3"]]->quizzes = array();
+$student->courses[$_POST["course3"]]->aweight = $_POST[];
+$student->courses[$_POST["course3"]]->eweight = $_POST[];
+$student->courses[$_POST["course3"]]->qweight = $_POST[];
+
+$student->courses[$_POST["course4"]]->assignments = array();
+$student->courses[$_POST["course4"]]->exams = array();
+$student->courses[$_POST["course4"]]->quizzes = array();
+$student->courses[$_POST["course4"]]->aweight = $_POST[];
+$student->courses[$_POST["course4"]]->eweight = $_POST[];
+$student->courses[$_POST["course4"]]->qweight = $_POST[];
+
+$student->courses[$_POST["course5"]]->assignments = array();
+$student->courses[$_POST["course5"]]->exams = array();
+$student->courses[$_POST["course5"]]->quizzes = array();
+$student->courses[$_POST["course5"]]->aweight = $_POST[];
+$student->courses[$_POST["course5"]]->eweight = $_POST[];
+$student->courses[$_POST["course5"]]->qweight = $_POST[];
+
+$student->courses[$_POST["course6"]]->assignments = array();
+$student->courses[$_POST["course6"]]->exams = array();
+$student->courses[$_POST["course6"]]->quizzes = array();
+$student->courses[$_POST["course6"]]->aweight = $_POST[];
+$student->courses[$_POST["course6"]]->eweight = $_POST[];
+$student->courses[$_POST["course6"]]->qweight = $_POST[];
+
 $userData = fopen("userData.json", "a+");
 fwrite($userData, json_encode($student));
 fclose($userData);
