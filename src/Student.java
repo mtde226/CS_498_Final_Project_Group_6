d61c5e8a888cf0dfd2fe4ed287b2cd15b0b5d25a
tjdi226 <47254474+tjdi226@users.noreply.github.com>
2019-04-10 12:22:25 -0400

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,5 +1,4 @@
 package gradebook;
-
 import java.util.*;
 
 public class Student {
@@ -7,7 +6,7 @@ public class Student {
 	public String name;	
 	public ArrayList<course> courseList = new ArrayList<course>();
 	public void printStudentInfo() {//Will eventually print the entire set of student's assignments
-		System.out.print("\nThe student's name is:");
+		System.out.print("\nThe student's name is: ");
 		System.out.print(name);
 		for (int i = 0; i < courseList.size(); i++) {
 			courseList.get(i).print();
